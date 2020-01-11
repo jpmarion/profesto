9883d1e7e173f3181217177ea0958d32657260dd
Juan Pablo Marión <totomarion@gmail.com>
2020-01-11 10:39:03 -0300

    RF5 Devolver Datos Usuario

--- back/app/Http/Controllers/API/AuthController.php
+++ back/app/Http/Controllers/API/AuthController.php
@@ -21,10 +21,9 @@ use Illuminate\Support\Facades\Auth;
 /**
  *  @OA\SecurityScheme(
  *      securityScheme="bearerAuth",
- *         type="http",
- *        scheme="bearer",
- *       bearerFormat="JWT"
- *  ),
+ *      type="http",
+ *      scheme="bearer",
+ *  )
  */
 
 class AuthController extends Controller
@@ -182,6 +181,47 @@ class AuthController extends Controller
         ]);
     }
 
+    /**
+     * @OA\Get(
+     *      path="/api/auth/logout",
+     *      tags={"AuthController"},
+     *      summary="Logout de usuario",
+     *      operationId="logout",
+     *      security={{"bearerAuth":{}}},
+     *     
+     *      @OA\Response(
+     *          response=201,
+     *          description="Usuario creado",
+     *          @OA\MediaType(
+     *              mediaType="application/json",
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=400,
+     *          description="Solicitud no válida"
+     *      ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="No autorizado"
+     *      ),     
+     *      @OA\Response(
+     *          response=404,
+     *          description="No encontrado"
+     *      ),
+     *      @OA\Response(
+     *          response=422,
+     *          description="Error validación"
+     *      )
+     *  )
+     */
+    public function logout(Request $request)
+    {
+        $request->user()->token()->revoke();
+        return response()->json([
+            'message' => 'Salió exitosamente'
+        ]);
+    }
+
     public function registerActivate($token)
     {
         $user = User::where('activation_token', $token)->first();
@@ -193,4 +233,42 @@ class AuthController extends Controller
         $user->save();
         return $user;
     }
+
+    /**
+     * @OA\Get(
+     *      path="/api/auth/user",
+     *      tags={"AuthController"},
+     *      summary="Datos del usuario",
+     *      operationId="user",
+     *      security={{"bearerAuth":{}}},
+     *     
+     *      @OA\Response(
+     *          response=201,
+     *          description="Usuario",
+     *          @OA\MediaType(
+     *              mediaType="application/json",
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=400,
+     *          description="Solicitud no válida"
+     *      ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="No autorizado"
+     *      ),     
+     *      @OA\Response(
+     *          response=404,
+     *          description="No encontrado"
+     *      ),
+     *      @OA\Response(
+     *          response=422,
+     *          description="Error validación"
+     *      )
+     *  )
+     */
+    public function user(Request $request){
+        return response()->json($request->user());
+    }
+
 }
