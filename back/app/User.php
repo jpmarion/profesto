449035d38698276c153f3f4233521c4fd3438767
Juan Pablo Marión <totomarion@gmail.com>
2020-02-15 11:45:22 -0300

    Agregar Swagger a User

--- back/app/User.php
+++ back/app/User.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Passport\HasApiTokens;
 
+
 class User extends Authenticatable
 {
     use HasApiTokens, Notifiable, SoftDeletes;
@@ -40,4 +41,23 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     *
+     * @OA\Schema(
+     *     schema="User",
+     *     title="User",
+     *     description="User representation",
+     *     @OA\Property(type="integer", property="id", description="The user id"),
+     *     @OA\Property(type="string", property="name", description="Nombre del usuario"),
+     *     @OA\Property(type="string", property="email", format="email", description="Email del usuario"),
+     *     @OA\Property(type="string", format="date-time", property="email_verified_at", description="Cuando el usuario verifica su email", nullable=true),
+     *     @OA\Property(type="boolean", property="active", description="Si usuario se encuentra activo"),
+     *     @OA\Property(type="string", format="date-time", property="created_at", description="Cuando el usuario fue creado"),
+     *     @OA\Property(type="string", format="date-time", property="updated_at", description="Cuando el usuario fue modificado la última vez"),
+     *     @OA\Property(type="string", format="date-time", property="deleted_at", description="When user was last updated"),
+     *     @OA\Property(type="string", property="email_alternativo", format="email", description="Email alternativo del usuario")
+     * )
+     *
+     */
 }
