0d0d1bcec6e8599a07f965643423b7061f0a411f
Juan Pablo Marión <totomarion@gmail.com>
2020-02-21 08:49:40 -0300

    Test AuthController

--- back/tests/Feature/AuthTest.php
+++ back/tests/Feature/AuthTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Notification;
 use Tests\TestCase;
 
 class AuthTest extends TestCase
