ba79bc55909ffe94b3fbed2ce75ed16895c2b764
Juan Pablo Marión <totomarion@gmail.com>
2020-03-27 11:40:52 -0300

    Crear auth module y routing

--- front/src/app/pages/auth/auth.module.ts
+++ front/src/app/pages/auth/auth.module.ts
@@ -1,23 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
+import { AuthRoutingModule } from './auth-routing.module';
+
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatGridListModule,
-    MatCheckboxModule,
-    MatButtonModule,
-    MatCardModule
+    AuthRoutingModule
   ]
 })
 export class AuthModule { }
