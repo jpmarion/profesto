1a40d5ec1e418d7ed3944a67534fb866ee85c91e
Juan Pablo Marión <totomarion@gmail.com>
2020-03-27 10:48:27 -0300

    Corregir código vuelta atrás

---
+++ front/src/app/pages/auth/auth.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatGridListModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    MatCardModule
+  ]
+})
+export class AuthModule { }
